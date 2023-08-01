d287e9e9012dc011e0841986e06b23fe9662118c
MatthiasScreed <christopher.massamba@gmail.com>
2023-08-01 12:19:12 +0200

    création blade post

---
+++ resources/views/posts/post.blade.php
@@ -0,0 +1,11 @@
+<x-front.layout>
+    <article>
+        <h1>{{ $post->title }}</h1>
+
+        <div>
+            {!! $post->body !!}
+        </div>
+    </article>
+
+    <a href="/">Go Back</a>
+</x-front.layout>
---
+++ resources/views/posts/posts.blade.php
@@ -0,0 +1,4 @@
+<x-front.layout>
+    hello world
+    hello world
+</x-front.layout>
