934aee4f7e9beca0273ac5c197c873efec541787
MatthiasScreed <christopher.massamba@gmail.com>
2024-02-29 12:27:05 +0100

    deployementsoon

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,8 +22,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call(LaratrustSeeder::class);
         $this->call(UsersTableSeeder::class);
-        $this->call(CategoryTableSeeder::class);
-        $this->call(PostsTableSeeder::class);
+//        $this->call(CategoryTableSeeder::class);
+//        $this->call(PostsTableSeeder::class);
 
     }
 }
