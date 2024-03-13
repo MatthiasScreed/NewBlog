1f68af00cffd8785a938b594a6ce403420991edf
MatthiasScreed <christopher.massamba@gmail.com>
2024-03-13 16:53:23 +0100

    frontissue

--- resources/views/components/front/layout.blade.php
+++ resources/views/components/front/layout.blade.php
@@ -33,9 +33,9 @@
     <div class="min-h-screen bg-gray-100">
         <x-front.navigation/>
 
-        <div>
+        <main>
             {{ $slot }}
-        </div>
+        </main>
 
         <footer id="newsletter" class="py-16 px-10 mt-16 text-center bg-white">
             <img src="{{asset('test_Logo.png')}}"
